eb732a5906982f47787e0791030c86a233f9f478
EleqfulCEAC <elean.quintero@alu.ceacfp.es>
2025-02-13 13:52:59 +0100

    cambios en creditcardpayment

--- src/tiendaOnline/creditCardPayment.java
+++ src/tiendaOnline/creditCardPayment.java
@@ -1,5 +1,40 @@
 package tiendaOnline;
 
-public class creditCardPayment {
-
+public class creditCardPayment implements metodosDePago {
+	private String numeroTarjeta; 
+	private double limiteDisponible;
+	
+	
+	
+	
+	
+	public creditCardPayment(String numeroTarjeta, double limiteDisponible) {
+		super();
+		this.numeroTarjeta = numeroTarjeta;
+		this.limiteDisponible = limiteDisponible;
+	}
+	@Override
+	public boolean validarPago(double cantidad) {
+			if(cantidad < this.limiteDisponible) {
+				return true;
+			} else return false;
+	
+	}
+	@Override
+	public void procesarPago(double cantidad) {
+		if(validarPago(cantidad)) {
+			this.limiteDisponible =- cantidad; 
+			System.out.println("El restante es: " + this.limiteDisponible);
+		}
+		
+	}
+	@Override
+	public void mostrarInfo() {
+		System.out.println("Datos del metodo de pago: ");
+		System.out.println("Numero de la tarjeta: " + numeroTarjeta);
+		System.out.println("Limite actual de la tarjeta: " + limiteDisponible);
+		
+	} 
+	
+	
 }
